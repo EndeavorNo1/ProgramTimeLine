473edc8e69d3cf61e1b09a92dbca467ccb5f98cc
慧新 <31230553+EndeavorNo1@users.noreply.github.com>
2022-11-05 21:32:06 +0800

    feat: 样式更改

--- src/TimeLineRow.tsx
+++ src/TimeLineRow.tsx
@@ -5,37 +5,37 @@ const TimeLineRow = () => {
       <h1>Timeline</h1>
       <ul className="timeline-events">
         <li className="timeline-event-years-2">
-          <h2>2004-2006</h2>
-          <h3>Ideaworks Advertising</h3>
-          <h4>Interactive Designer</h4>
+          <h2>10-08～10-18</h2>
+          <h3>6.0.3调研</h3>
+          <h4>产品：xxx</h4>
         </li>
         <li className="timeline-event-years-2">
-          <h2>2006-2008</h2>
-          <h3>Pint, Inc</h3>
-          <h4>Associate Creative Director</h4>
+          <h2>10-18～10-20</h2>
+          <h3>6.0.3设计稿</h3>
+          <h4>UI：xxx</h4>
         </li>
         <li className="timeline-event-years-3">
-          <h2>2008-2011</h2>
-          <h3>Cuker Interactive</h3>
-          <h4>Creative</h4>
+          <h2>10-21～10-30</h2>
+          <h3>6.0.3接口开发</h3>
+          <h4>后端：xxx</h4>
         </li>
         <li className="timeline-event-years-7">
-          <h2>2011-Present</h2>
-          <h3>Independent</h3>
-          <h4>Interactive Art Director</h4>
+          <h2>11-01～11-30</h2>
+          <h3>6.0.3接口开发</h3>
+          <h4>前端：xxx</h4>
         </li>
       </ul>
       <ul className="timelines-years">
-        <li>2000</li>
-        <li>2002</li>
-        <li>2004</li>
-        <li>2006</li>
-        <li>2008</li>
-        <li>2010</li>
+        <li>2022/10/18</li>
+        <li>2022/10/21</li>
+        <li>2022/10/24</li>
+        <li>2022/11/30</li>
+        <li>2022/12/08</li>
+        {/*<li>2010</li>
         <li>2012</li>
         <li>2014</li>
         <li>2016</li>
-        <li>2018</li>
+        <li>2018</li> */}
       </ul>
     </div>
   );
